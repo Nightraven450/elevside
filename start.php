d121af1452cfbe9bf48956db9f08bf27b148973c
jeppeeeee <heltjeppe@gmail.com>
2025-01-23 11:38:39 +0100

    lol

---
+++ start.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start Page</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <div class="container">
+        <img src="images/1000001813.jpg" alt="Start Image" style="width: 100%; height: auto;">
+        <a href="pages/login.php" class="login-button">Login</a>
+    </div>
+</body>
+</html>
+
